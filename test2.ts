1990ee87d0d244806b4c303d6802434ea7276c37
Juan <juan@onprotec.com.com>
2026-08-13 20:45:31 -0400

    fix: remove test ts files properly

--- test2.ts
+++
@@ -1,1 +0,0 @@
-const { image_search } = require('duckduckgo-images-api'); image_search({ query: 'TCL 43F35', moderate: true }).then(results => console.log(results[0]?.image)).catch(console.error);
--- test3.ts
+++
@@ -1,1 +0,0 @@
-const google = require('googlethis'); google.image('TCL 43F35', { safe: false }).then(res => console.log(res[0]?.url)).catch(console.error);
